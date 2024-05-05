525c87fd65b1ab04174d8c9acc505b19e9bca80d
JosueMachadoJr <161335190+JosueMachadoJr@users.noreply.github.com>
2024-05-05 12:40:16 -0300

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,18 @@
+
+import { NativeBaseProvider, StatusBar } from 'native-base';
+import { TEMAS } from './src/Estilos/temas';
+import Rotas from './src/Rotas';
+
+
+
+
+export default function App() {
+  return (
+    <NativeBaseProvider theme={TEMAS}>
+      <StatusBar backgroundColor={TEMAS.colors.blue[800]} />
+      <Rotas/>
+    </NativeBaseProvider>
+    
+  );
+}
+
